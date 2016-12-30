8e06ba0c48e4eac560ebbf7aae75c6692ad33bd4
Pallela Venkat Karthik <pallela_karthik@yahoo.com>
2016-12-30 12:21:26 +0530

    Added some extra code for processing few more requests currently everything is dummy

--- vhostnetpci_test.c
+++ vhostnetpci_test.c
@@ -6,6 +6,57 @@
 #include<stdlib.h>
 #include"virtio.h"
 
+#define VHOST_USER_VERSION    0x1
+
+#define VHOST_USER_F_PROTOCOL_FEATURES  30
+
+#define VHOST_SUPPORTS_MQ      (1ULL << VIRTIO_NET_F_MQ)
+#define VHOST_F_LOG_ALL 26
+/* Features supported by this lib. */
+
+#if 0
+#define VHOST_SUPPORTED_FEATURES ((1ULL << VIRTIO_NET_F_MRG_RXBUF) | \
+                                (1ULL << VIRTIO_NET_F_CTRL_VQ) | \
+                                (1ULL << VIRTIO_NET_F_CTRL_RX) | \
+                                (1ULL << VIRTIO_NET_F_GUEST_ANNOUNCE) | \
+                                (VHOST_SUPPORTS_MQ)            | \
+                                (1ULL << VIRTIO_F_VERSION_1)   | \
+                                (1ULL << VHOST_F_LOG_ALL)      | \
+                                (1ULL << VHOST_USER_F_PROTOCOL_FEATURES) | \
+                                (1ULL << VIRTIO_NET_F_HOST_TSO4) | \
+                                (1ULL << VIRTIO_NET_F_HOST_TSO6) | \
+                                (1ULL << VIRTIO_NET_F_CSUM)    | \
+                                (1ULL << VIRTIO_NET_F_GUEST_CSUM) | \
+                                (1ULL << VIRTIO_NET_F_GUEST_TSO4) | \
+                                (1ULL << VIRTIO_NET_F_GUEST_TSO6))
+#endif
+
+
+
+#define VHOST_SUPPORTED_FEATURES ((1ULL << VIRTIO_NET_F_MRG_RXBUF) | \
+                                (1ULL << VIRTIO_NET_F_CTRL_VQ) | \
+                                (1ULL << VIRTIO_NET_F_CTRL_RX) | \
+                                (1ULL << VIRTIO_NET_F_GUEST_ANNOUNCE) | \
+                                (1ULL << VIRTIO_F_VERSION_1)   | \
+                                (1ULL << VHOST_F_LOG_ALL)      | \
+                                (1ULL << VHOST_USER_F_PROTOCOL_FEATURES) | \
+                                (1ULL << VIRTIO_NET_F_HOST_TSO4) | \
+                                (1ULL << VIRTIO_NET_F_HOST_TSO6) | \
+                                (1ULL << VIRTIO_NET_F_CSUM)    | \
+                                (1ULL << VIRTIO_NET_F_GUEST_CSUM) | \
+                                (1ULL << VIRTIO_NET_F_GUEST_TSO4) | \
+                                (1ULL << VIRTIO_NET_F_GUEST_TSO6))
+
+#define VHOST_USER_PROTOCOL_F_MQ        0
+#define VHOST_USER_PROTOCOL_F_LOG_SHMFD 1
+#define VHOST_USER_PROTOCOL_F_RARP      2
+
+#define VHOST_USER_PROTOCOL_FEATURES    ((1ULL << VHOST_USER_PROTOCOL_F_MQ) | \
+                                         (1ULL << VHOST_USER_PROTOCOL_F_LOG_SHMFD) |\
+                                         (1ULL << VHOST_USER_PROTOCOL_F_RARP))
+
+
+
 #define SOCKPATH "/usr/local/var/run/openvswitch/vhost-user1"
 
 void print_hex(unsigned char *buff, int len)
@@ -73,35 +124,96 @@ main()
 
 	while(1) {
 
-		recv_bytes = recv(newsockfd,rxbuff,100,0);
-		printf("received bytes : %d\n",recv_bytes);
-		print_hex(rxbuff,recv_bytes);
+		recv_bytes = recv(newsockfd,rxbuff,12,0);
 
 		msg = (struct vhost_user_msg *) rxbuff;
 
-		printf("msg.request : %d name : %s\n",msg->request,req_names[msg->request]);
+		if(msg->size != 0) {
+			recv_bytes += recv(newsockfd,rxbuff+12,msg->size,0);
+		}
+
+
+		printf("received total bytes : %d\n",recv_bytes);
+
+		print_hex(rxbuff,recv_bytes);
+
+
+		printf("msg.request : %d name : %s payload size : %d\n",msg->request,req_names[msg->request],msg->size);
 
 		switch(msg->request) {
 
 			case VHOST_USER_GET_FEATURES:
 				memcpy(txbuff,rxbuff,recv_bytes);
 				msg = (struct vhost_user_msg *) txbuff;
-				msg->payload.u64 = (1ULL << 30);
+				//msg->payload.u64 = (1ULL << 30);
+				msg->payload.u64 = VHOST_SUPPORTED_FEATURES;
 				msg->size = 8;
 				msg->flags &= ~VHOST_USER_VERSION_MASK;
-				msg->flags |= 0x1;
+				msg->flags |= VHOST_USER_VERSION;
 				msg->flags |= VHOST_USER_REPLY_MASK;
 				send(newsockfd,txbuff,20,0);
 				printf("sent bytes : %d\n",20);
 				print_hex(txbuff,20);
 
 			break;
+
+			case VHOST_USER_SET_FEATURES:
+			memcpy(txbuff,rxbuff,recv_bytes);
+			msg = (struct vhost_user_msg *) txbuff;
+			printf("payload is %016x\n",msg->payload.u64);
+			break;
+			
+
+			case  VHOST_USER_GET_PROTOCOL_FEATURES:
+			memcpy(txbuff,rxbuff,recv_bytes);
+			msg = (struct vhost_user_msg *) txbuff;
+			msg->payload.u64 = VHOST_USER_PROTOCOL_FEATURES;
+			msg->size = 8;
+			msg->flags &= ~VHOST_USER_VERSION_MASK;
+			msg->flags |= VHOST_USER_VERSION;
+			msg->flags |= VHOST_USER_REPLY_MASK;
+			send(newsockfd,txbuff,20,0);
+			printf("\n\nsent bytes : %d\n",20);
+			print_hex(txbuff,20);
+			printf("\n\n");
+
+			break;
+
+			case VHOST_USER_GET_QUEUE_NUM:
+			memcpy(txbuff,rxbuff,recv_bytes);
+			msg = (struct vhost_user_msg *) txbuff;
+			msg->payload.u64 = 0x8000;
+			msg->size = 8;
+			msg->flags &= ~VHOST_USER_VERSION_MASK;
+			msg->flags |= VHOST_USER_VERSION;
+			msg->flags |= VHOST_USER_REPLY_MASK;
+			send(newsockfd,txbuff,20,0);
+			printf("\n\nsent bytes : %d\n",20);
+			print_hex(txbuff,20);
+			printf("\n\n");
+			break;
+
+			case VHOST_USER_SET_VRING_CALL:
+			memcpy(txbuff,rxbuff,recv_bytes);
+			msg = (struct vhost_user_msg *) txbuff;
+			printf("payload : %llx\n",msg->payload.u64);
+			break;
+
+			case VHOST_USER_SET_VRING_ENABLE:
+			memcpy(txbuff,rxbuff,recv_bytes);
+			msg = (struct vhost_user_msg *) txbuff;
+			printf("payload.state.index : %u\n",msg->payload.state.index);
+			printf("payload.state.num   : %u\n",msg->payload.state.num);
+			break;
+			
+
+
 			default:
 			printf("default\n");
 		}
 
 	}
-	
+
 
 
 
--- virtio.h
+++ virtio.h
@@ -4,6 +4,45 @@
 
 #include<stdint.h>
 
+
+/* The feature bitmap for virtio net */
+#define VIRTIO_NET_F_CSUM       0       /* Host handles pkts w/ partial csum */
+#define VIRTIO_NET_F_GUEST_CSUM 1       /* Guest handles pkts w/ partial csum */
+#define VIRTIO_NET_F_MAC        5       /* Host has given MAC address. */
+#define VIRTIO_NET_F_GUEST_TSO4 7       /* Guest can handle TSOv4 in. */
+#define VIRTIO_NET_F_GUEST_TSO6 8       /* Guest can handle TSOv6 in. */
+#define VIRTIO_NET_F_GUEST_ECN  9       /* Guest can handle TSO[6] w/ ECN in. */
+#define VIRTIO_NET_F_GUEST_UFO  10      /* Guest can handle UFO in. */
+#define VIRTIO_NET_F_HOST_TSO4  11      /* Host can handle TSOv4 in. */
+#define VIRTIO_NET_F_HOST_TSO6  12      /* Host can handle TSOv6 in. */
+#define VIRTIO_NET_F_HOST_ECN   13      /* Host can handle TSO[6] w/ ECN in. */
+#define VIRTIO_NET_F_HOST_UFO   14      /* Host can handle UFO in. */
+#define VIRTIO_NET_F_MRG_RXBUF  15      /* Host can merge receive buffers. */
+#define VIRTIO_NET_F_STATUS     16      /* virtio_net_config.status available */
+#define VIRTIO_NET_F_CTRL_VQ    17      /* Control channel available */
+#define VIRTIO_NET_F_CTRL_RX    18      /* Control channel RX mode support */
+#define VIRTIO_NET_F_CTRL_VLAN  19      /* Control channel VLAN filtering */
+#define VIRTIO_NET_F_CTRL_RX_EXTRA 20   /* Extra RX mode control support */
+#define VIRTIO_NET_F_GUEST_ANNOUNCE 21  /* Guest can announce device on the
+                                         * network */
+#define VIRTIO_NET_F_MQ         22      /* Device supports Receive Flow
+                                         * Steering */
+#define VIRTIO_NET_F_CTRL_MAC_ADDR 23   /* Set MAC address */
+
+/* Do we get callbacks when the ring is completely used, even if we've
+ * suppressed them? */
+#define VIRTIO_F_NOTIFY_ON_EMPTY        24
+
+/* Can the device handle any descriptor layout? */
+#define VIRTIO_F_ANY_LAYOUT             27
+
+/* We support indirect buffer descriptors */
+#define VIRTIO_RING_F_INDIRECT_DESC     28
+
+#define VIRTIO_F_VERSION_1              32
+
+
+
 enum vhost_user_request {
         VHOST_USER_NONE = 0,
         VHOST_USER_GET_FEATURES = 1,
